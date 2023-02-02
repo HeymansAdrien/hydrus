22106d3a26de1e90ce71636067057a625fb1acf9
HeymansAdrien <36161856+HeymansAdrien@users.noreply.github.com>
2023-02-02 15:58:56 +0100

    SetSkV

---
+++ CMakeFiles/h1d_calc.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/HYDRUS.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/HYDRUS.FOR.o"
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/HYSTER.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/HYSTER.FOR.o"
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/INPUT.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/INPUT.FOR.o"
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/MATERIAL.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/MATERIAL.FOR.o"
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/OUTPUT.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/OUTPUT.FOR.o"
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/SINK.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/SINK.FOR.o"
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/SOLUTE.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/SOLUTE.FOR.o"
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/TEMPER.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/TEMPER.FOR.o"
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/TIME.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/TIME.FOR.o"
+  "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/WATFLOW.FOR" "/mnt/c/Users/heymansad/Documents/GitHub/Hydrus/CMakeFiles/h1d_calc.dir/WATFLOW.FOR.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_Fortran
+  "GFORTRAN"
+  )
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
